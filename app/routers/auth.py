7876614bea4ff64b136d1e34cdac8befc672c3fc
tatnik <ptn48@yandex.ru>
2025-05-18 12:43:55 +0300

    refact auth:  rename utils to auth_utils

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -5,15 +5,15 @@ from sqlalchemy.orm import Session
 
 import app.schemas as schemas
 import app.models as models
-import app.utils as utils
+import app.auth_utils as auth_utils
 from app.core.config import settings
 
 # --- Константы и зависимости
 router = APIRouter(tags=["Auth"])
 
-oauth2_scheme = utils.oauth2_scheme
-get_db = utils.get_db
-credentials_exception = utils.credentials_exception
+oauth2_scheme = auth_utils.oauth2_scheme
+get_db = auth_utils.get_db
+credentials_exception = auth_utils.credentials_exception
  
 # --- Маршруты авторизации      
 @router.post("/register", response_model=schemas.UserRead)
@@ -29,7 +29,7 @@ def register(
             detail="Email already registered"
         )
     
-    hashed = utils.get_password_hash(user_in.password)
+    hashed = auth_utils.get_password_hash(user_in.password)
     new_user = models.User(email=user_in.email, hashed_password=hashed)
     db.add(new_user)
     db.commit()
@@ -42,27 +42,27 @@ def login(
     db: Session = Depends(get_db)
 ):
     user = db.query(models.User).filter(models.User.email == form_data.username).first()
-    if not user or not utils.verify_password(form_data.password, user.hashed_password):
+    if not user or not auth_utils.verify_password(form_data.password, user.hashed_password):
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Incorrect email or password",
             headers={"WWW-Authenticate": "Bearer"}
         )
     access_token_expires = timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
-    access_token = utils.create_access_token(data={"sub": user.email}, expires_delta=access_token_expires)
+    access_token = auth_utils.create_access_token(data={"sub": user.email}, expires_delta=access_token_expires)
     return schemas.Token(access_token=access_token, token_type="bearer")
 
 @router.post("/logout", status_code=status.HTTP_204_NO_CONTENT)
 def logout(
-    token: str = Depends(utils.oauth2_scheme)
+    token: str = Depends(auth_utils.oauth2_scheme)
 ):
     """Отзывает токен и добавляет его в черный список."""
-    utils.revoke_token(token)
+    auth_utils.revoke_token(token)
     return
 
 @router.get("/me", response_model=schemas.UserRead)
 def read_users_me(
-    current_user: models.User = Depends(utils.get_current_user)
+    current_user: models.User = Depends(auth_utils.get_current_user)
 ):
     """Информация о текущем пользователе"""
     return current_user
